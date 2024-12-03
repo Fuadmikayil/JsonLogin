c6d8283a54ace952aa59cdb36e989460ac58c4f6
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-03 13:13:55 +0400

    createPost func

--- src/User/user.jsx
+++ src/User/user.jsx
@@ -1,25 +1,24 @@
 import React, { useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { useParams } from "react-router-dom";
-import "./user.css"
+import "./user.css";
 const User = () => {
   const { user } = useParams();
-    const [doesCreate ,setdoesCreate] = useState(false)
+  const [doesCreate, setdoesCreate] = useState(false);
   return (
     <div className="user-area">
-      <div className="user-post">
-
-      </div>
+      <div className="user-post"></div>
       <div className="create-post">
-        <FiPlus className="fi-plus"/>
+        <FiPlus className="fi-plus" />
       </div>
-    {
-        doesCreate ?   <div className="post-text">
-        <textarea name="" id=""></textarea>
-        <button onClick={sendPost} ></button>
-      </div>:
-      <></>
-    }
+      {doesCreate ? (
+        <div className="post-text">
+          <textarea name="" id=""></textarea>
+          <button onClick={sendPost}></button>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };

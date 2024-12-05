5b55da7a67fb1c60fa176eddbd2f9ecfe0502975
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-05 13:06:30 +0400

    css add

--- src/User/user.jsx
+++ src/User/user.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { FiPlus } from "react-icons/fi";
 import { useParams } from "react-router-dom";
-
+import "./user.css"
 const User = () => {
   const { user } = useParams();
 
   return (
     <div className="user-area">
-      <div className="user-post"></div>
+      <div className="user-post">
+
+      </div>
       <div className="create-post">
         <FiPlus className="fi-plus"/>
       </div>

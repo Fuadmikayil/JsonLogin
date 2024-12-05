b610828179be3b6b842f75229a009f44b632ad86
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-05 10:04:43 +0400

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
-import FirstPage from './firstPage/firstPage'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import FirstPage from "./firstPage/firstPage";
+import User from "./User/user";
 
 function App() {
-
   return (
     <section>
       <FirstPage />
+      <Routes>
+        <Route path="/" elemen={<FirstPage />} />
+        <Route path="/:user" elemen={<User />} />
+      </Routes>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;

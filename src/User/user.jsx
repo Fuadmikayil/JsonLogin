2524177c75a67945a1606903ff484b2328728176
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-06 09:42:41 +0400

    Data add

--- src/User/user.jsx
+++ src/User/user.jsx
@@ -2,23 +2,33 @@ import React, { useState } from "react";
 import { FiPlus } from "react-icons/fi";
 import { useParams } from "react-router-dom";
 import "./user.css";
+import axios from "axios";
 const User = () => {
   const { user } = useParams();
   const [doesCreate, setdoesCreate] = useState(false);
+  const createPost = () => {
+    setdoesCreate(!doesCreate);
+  };
+  const sendPost = async() => {
+    const userPost = {
+        userName:user,
+        userPost:document.getElementsByTagName("textarea")[0].value
+    }
+    await(await axios.post("http://localhost:3000/userPosts",userPost)).data
+  };
   return (
     <div className="user-area">
       <div className="user-post"></div>
       <div className="create-post">
-        <FiPlus className="fi-plus" />
+        <FiPlus className="fi-plus" onClick={createPost} />
       </div>
-      {doesCreate ? (
-        <div className="post-text">
+      {doesCreate ?  <div className="post-text">
           <textarea name="" id=""></textarea>
-          <button onClick={sendPost}></button>
+          <button onClick={sendPost}>Post</button>
         </div>
-      ) : (
+       : 
         <></>
-      )}
+      }
     </div>
   );
 };

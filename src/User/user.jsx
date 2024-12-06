178dbfa095b22aab0f9bfb6ca4f97318c5d2b613
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-12-06 10:05:37 +0400

    onClick

--- src/User/user.jsx
+++ src/User/user.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FiPlus } from "react-icons/fi";
+import { FiMinus, FiPlus } from "react-icons/fi";
 import { useParams } from "react-router-dom";
 import "./user.css";
 import axios from "axios";
@@ -19,8 +19,13 @@ const User = () => {
   return (
     <div className="user-area">
       <div className="user-post"></div>
-      <div className="create-post">
-        <FiPlus className="fi-plus" onClick={createPost} />
+      <div className="create-post" onClick={createPost}>
+        {
+          !doesCreate ?
+          <FiPlus className="fi-plus" />
+          :
+          <FiMinus className="fi-plus"  />
+        }
       </div>
       {doesCreate ?  <div className="post-text">
           <textarea name="" id=""></textarea>
